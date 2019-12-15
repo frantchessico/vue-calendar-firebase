20b14493ffe1cf8564247bd848f43075184893fb
frantchessico <jaimeinoque20@gmail.com>
2019-12-15 17:20:30 +0200

    atualizei main.js

--- src/main.js
+++ src/main.js
@@ -9,14 +9,13 @@ Vue.use(VueTextareaAutosize)
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  apiKey: "AIzaSyA8Oupv4KGl2mXQz79KM35zNslW6qCha8g",
-  authDomain: "franciscoinoqueportfolio.firebaseapp.com",
-  databaseURL: "https://franciscoinoqueportfolio.firebaseio.com",
-  projectId: "franciscoinoqueportfolio",
-  storageBucket: "franciscoinoqueportfolio.appspot.com",
-  messagingSenderId: "1053727144077",
-  appId: "1:1053727144077:web:350ad95394ebb47e114ac4",
-  measurementId: "G-L286FW9MEX"
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#",
+    appId: "#"
 });
 
 export const db = firebase.firestore();
